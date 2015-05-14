64b52b649cfdc32c04a9783c0363ced9df70eabb
LiesbethVanaerschot <lies.vanaerschot@gmail.com>
2015-05-14 16:52:00 +0200

    mobile menu sliding

--- sites/all/themes/areau/js/scripts.js
+++ sites/all/themes/areau/js/scripts.js
@@ -4,17 +4,46 @@
         attach: function (context, settings) {
             // Get your Yeti started.
 
-            /*$(document).ready(function () {
-                var width = $(window).width();
+            $(document).ready(function () {
+                console.log('ready');
+                $('.menu-719').append("<img class='hamburger' src='sites/all/themes/areau/images/mobile.png' />");
+                
+                var li1 = $('.menu-718').hide();
+                var li2 = $('.menu-510').hide();
+                var li3 = $('.menu-511').hide();
+                var li5 = $('.menu-512').hide();
+                var li6 = $('.menu-513').hide();
+                var li7 = $('.menu-514').hide();
+                
+                $('.hamburger').on('click', function(){
+                    if($(this).hasClass('open') == false)
+                    {
+                        $(this).addClass('open');
+                        $(this).css({'opacity':0.6});
+                        li1.slideDown('slow');
+                        li2.slideDown('slow');
+                        li3.slideDown('slow');
+                        li5.slideDown('slow');
+                        li6.slideDown('slow');
+                        li7.slideDown('slow'); 
+                    }
+                    else if($(this).hasClass('open'))
+                    {
+                        $(this).removeClass('open');
+                        li1.slideUp('slow');
+                        li2.slideUp('slow');
+                        li3.slideUp('slow');
+                        li5.slideUp('slow');
+                        li6.slideUp('slow');
+                        li7.slideUp('slow');
+                        $(this).css({'opacity':1}); 
+                    }
+                    
+                });
+                
+                
 
-                if (width <= 768) {
-                    $('.menu-671').css("display", "none");
-                } else {
-                    $('.menu-719').css("display", "none");
-                }
-
-
-            });*/
+            });
         }
     };
 
