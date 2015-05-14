71e565dbe2b0d3d34987fbfa2d8807aae306c71c
LiesbethVanaerschot <lies.vanaerschot@gmail.com>
2015-05-14 17:06:26 +0200

    mobile menu gefixt

--- sites/all/themes/areau/js/scripts.js
+++ sites/all/themes/areau/js/scripts.js
@@ -8,14 +8,14 @@
                 console.log('ready');
                 $('.menu-719').append("<img class='hamburger' src='sites/all/themes/areau/images/mobile.png' />");
                 
-                var li1 = $('.menu-718').hide();
-                var li2 = $('.menu-510').hide();
-                var li3 = $('.menu-511').hide();
-                var li5 = $('.menu-512').hide();
-                var li6 = $('.menu-513').hide();
-                var li7 = $('.menu-514').hide();
+                    var li1 = $('.menu-718');
+                    var li2 = $('.menu-510');
+                    var li3 = $('.menu-511');
+                    var li5 = $('.menu-512');
+                    var li6 = $('.menu-513');
+                    var li7 = $('.menu-514');
                 
-                $('.hamburger').on('click', function(){
+                    $('.hamburger').on('click', function(){
                     if($(this).hasClass('open') == false)
                     {
                         $(this).addClass('open');
@@ -40,6 +40,60 @@
                     }
                     
                 });
+                
+                
+                checkWidth();
+                
+                $(window).resize(checkWidth);
+                
+                function checkWidth(){
+                var width = $(window).width();
+
+                if (width <= 768) {
+                    li1.hide();
+                    li2.hide();
+                    li3.hide();
+                    li5.hide();
+                    li6.hide();
+                    li7.hide();
+                    
+                } else {
+                    li1.show();
+                    li2.show();
+                    li3.show();
+                    li5.show();
+                    li6.show();
+                    li7.show();
+                    $('.hamburger').removeClass('open');
+                    $('.hamburger').css({'opacity':1}); 
+                }
+                }
+                
+                /*$('.hamburger').on('click', function(){
+                    if($(this).hasClass('open') == false)
+                    {
+                        $(this).addClass('open');
+                        $(this).css({'opacity':0.6});
+                        li1.slideDown('slow');
+                        li2.slideDown('slow');
+                        li3.slideDown('slow');
+                        li5.slideDown('slow');
+                        li6.slideDown('slow');
+                        li7.slideDown('slow'); 
+                    }
+                    else if($(this).hasClass('open'))
+                    {
+                        $(this).removeClass('open');
+                        li1.slideUp('slow');
+                        li2.slideUp('slow');
+                        li3.slideUp('slow');
+                        li5.slideUp('slow');
+                        li6.slideUp('slow');
+                        li7.slideUp('slow');
+                        $(this).css({'opacity':1}); 
+                    }
+                    
+                });*/
                 
                 
 
